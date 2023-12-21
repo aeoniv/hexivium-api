a4689428b4546f6f066b3368b6fb86eff7b80f81
Vinicius de Moraes Nascimento <38895221+aeoniv@users.noreply.github.com>
2023-12-21 17:17:41 +0000

    get

--- app.py
+++ app.py
@@ -6,24 +6,20 @@ app = Flask(__name__)  # Define the Flask app here
 
 # Replace 'your_api_key_here' with the actual API key
 #API_KEY = os.environ.get('BODYGRAPH') #, 'your_api_key_here'
-API_KEY = os.getenv("BODYGRAPH"), '86a17444-bc30-4cf5-a6f7-0d5f862a4ea9'
+API_KEY = os.getenv("BODYGRAPH"), 
 
-@app.route('/')
-def home():
-    return "Welcome to my Flask app!"
+#@app.route('/')
+#def home():
+#    return "Welcome to my Flask app!"
 
 # The route that your users will call
-@app.route('/fetch-data', methods=['GET'])
-def fetch_data():
-    # Extract parameters from the request
+@app.route('/fetch-bodygraph-data', methods=['GET'])
+def fetch_bodygraph_data():
+    # Fetch parameters from the incoming request
     date = request.args.get('date')
     timezone = request.args.get('timezone')
 
-    # Validate inputs
-    if not date or not timezone:
-        return jsonify({"error": "Missing date or timezone"}), 400
-
-    # Prepare the payload for the original API
+    url = "https://api.bodygraphchart.com/v221006/hd-data"
     payload = {
         'api_key': API_KEY,
         'date': date,
@@ -31,16 +27,19 @@ def fetch_data():
     }
 
     # Make a request to the original API
-    response = requests.post('https://api.bodygraphchart.com/v221006/hd-data', data=payload)
+    response = requests.get(url, params=payload)
 
-    # Check if the request was successful
-    if response.status_code != 200:
-        return jsonify({"error": "Failed to fetch data from the original API"}), 500
+    # Return the response as JSON
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({"error": "Failed to fetch data", "status_code": response.status_code})
 
-    # Return the data received from the original API
-    return jsonify(response.json())
+#if __name__ == '__main__':
+    # Only run the app if this script is executed directly
+#    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable
+#    app.run(host='0.0.0.0', port=port, debug=True)  # Start the Flask app
+
 
 if __name__ == '__main__':
-    # Only run the app if this script is executed directly
-    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable
-    app.run(host='0.0.0.0', port=port, debug=True)  # Start the Flask app
+    app.run(debug=True)
